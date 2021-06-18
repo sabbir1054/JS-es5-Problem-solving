fd793749102ecdd9d3a8dc74510086a3392a1c83
Your Name <you@example.com>
2021-06-18 10:18:55 +0600

    calculate multiply and division.

---
+++ 10-calculate-multiplication-division/script.js
@@ -0,0 +1,14 @@
+function multiply() {
+    number1 = document.getElementById("num1").value;
+    number2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = number1 * number2;
+
+    
+}
+
+function divide() {
+    number1 = document.getElementById("num1").value;
+    number2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = number1 / number2;
+
+} 
