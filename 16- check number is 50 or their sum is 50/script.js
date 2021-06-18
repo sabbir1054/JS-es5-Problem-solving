f76e7e4837ac4b9740ffe2961173279b170d89e8
Your Name <you@example.com>
2021-06-19 00:25:43 +0600

    check sepecific number.

---
+++ 16- check number is 50 or their sum is 50/script.js
@@ -0,0 +1,8 @@
+function test50(num1, num2) {
+    if ((num1 === 50 || num2 === 50)|| (num1+num2===50)) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+test50(20, 0);
