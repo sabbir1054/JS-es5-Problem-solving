4c6c128a8bf52395de09b88d96d83056713e6be3
Your Name <you@example.com>
2021-06-19 18:32:34 +0600

    Remove caracter from specific position.

---
+++ 21-Remove a Character form specific position/script.js
@@ -0,0 +1,10 @@
+function remove_character(str, char_pos) {
+    part1 = str.substring(0, char_pos);
+    part2 = str.substring(char_pos + 1, str.length);
+    return (part1 + part2);
+
+}
+
+console.log(remove_character("Python",0));// here remove p
+console.log(remove_character("Python",3));////here remove h
+console.log(remove_character("Muhammad Sabbir",8));//here remove space
