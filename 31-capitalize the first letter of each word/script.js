b2d31802a92510718507828ca26866979292c4c3
Your Name <you@example.com>
2021-06-20 00:25:59 +0600

    capitalized every first letter of word.

---
+++ 31-capitalize the first letter of each word/script.js
@@ -0,0 +1,16 @@
+ function cap_letter(str) 
+{
+    str = str.split(" ");
+
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+
+    return str.join(" ");
+}
+
+console.log(cap_letter("quick brown fox jumps over"));
+
+
+
+
