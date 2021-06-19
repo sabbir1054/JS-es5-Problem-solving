df3d323116db6c7491461c1915ce85da458ffa8c
Your Name <you@example.com>
2021-06-19 21:05:58 +0600

    Find number in Specific range.

---
+++ 24 - Check the number of specifuc range/script.js
@@ -0,0 +1,10 @@
+function check_number(num) {
+    if ((num >= 40 && num <= 60) || (num >= 70 && num <= 90)) {
+      console.log(true);
+    } else {
+        console.log(false);
+  }
+}
+
+check_number(45);
+check_number(65);// here when i use two peramitar then it check it 2 numbers
