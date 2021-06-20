8d56a1b66e78883b59cdd2479feea9b5c1a94522
Your Name <you@example.com>
2021-06-20 07:08:12 +0600

    Swap two halves of a given array.

---
+++ 36-Swap two halves of a given array/script.js
@@ -0,0 +1,16 @@
+function halv_array_swap(iarra) {
+    if (((iarra.length) % 2) != 0) {
+        return false;
+    } else {
+        for (var i = 0; i < iarra.length / 2; i++){
+            var temp = iarra[i];
+            iarra[i] = iarra[i+iarra.length/2];
+            iarra[i + iarra, length / 2] = temp;
+
+        }
+        return iarra;
+    }
+}
+
+console.log(halv_array_swap([1, 3, 5, 4, 6, 2]));
+console.log(halv_array_swap([1, 3, 5, 4, 2]));
