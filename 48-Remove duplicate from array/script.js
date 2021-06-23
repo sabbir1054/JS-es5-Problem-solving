9b4260bb30c201edce5bd054a46cccd62c6a4a9d
Your Name <you@example.com>
2021-06-23 14:18:11 +0600

    remove duplicate element from ARRAY.

---
+++ 48-Remove duplicate from array/script.js
@@ -0,0 +1,15 @@
+function findDuplicate(name) {
+     var uniqueName = [];
+    for (var i = 0; i < name.length; i++){
+        var element = name[i];
+        var index = uniqueName.indexOf(element);
+        if (index == -1) {
+            uniqueName.push(element);
+        }
+    }
+    return uniqueName;
+
+}
+
+var result = findDuplicate([5, 8, 74, 5, 2, 8, 5, 74, 8, 13, 17]);
+console.log(result);
